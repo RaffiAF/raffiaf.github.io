e004351887b339aeddb0a53edcb23d04bcfaacfc
RaffiAF <119820280+RaffiAF@users.noreply.github.com>
2025-03-05 12:44:45 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#text", {
+  strings: ["Information System Student", "Telkom University Student", "UI/UX Design"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
